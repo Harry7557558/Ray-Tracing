3c142879d11b7ca6270c7491b4bb6d35a637a91d
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2019-06-03 00:44:30 -0400

    Update RayTracing.cpp

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -1,28 +1,27 @@
 #include "World.h"
 using namespace std;
 
+#include "GlassMan.h"
 
 
-
-//#include "RayTracing2D.cpp"
 #include "Test.cpp"
+#include "CPT.cpp"
 
 #include <stdlib.h>
 #include <crtdbg.h>
 
 int main()
 {
-	//system("del /f /s /q D:\\Coding\\AboutPhysics\\RayTracing\\RayTracing\\Debug\\*.* /s");
-	//system("del /f /s /q D:\\Coding\\AboutPhysics\\RayTracing\\RayTracing\\Release\\*.* /s");
-
+	
 	fout << fixed << showpoint << setprecision(5);
 
 	_CrtSetBreakAlloc(-1);
-	Render_CTest03();
+	CPT_T1();
 	_CrtDumpMemoryLeaks();	// Visual Studio check memory leak
+
+	//pause;
 
 	return 0;
 
 }
 
-// https://github.com/miloyip/light2d
